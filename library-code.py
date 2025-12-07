55d85bdec2f50e5a927fc11fc752ebb5e5a6831e
Kristoffer B. Yddal <yddal@outlook.com>
2025-12-07 09:29:29 +0100

    Oppdatert klassen Library med planlagt funksjonalitet * Lå merke til at klassen movie og book kun inneholder de nye attributene og ikke attributene for LibraryItem klassen. Dette ble også lagt inn. * Gått igjennom og testet de forskjellige funksjonene for funksjon 1 til 6 i menyvalgene. * Jeg ser at det er kanskje noen funksjoner som kunne vært mer generelle ved å sjekke hvilken klasse en jobber med. Mulig en kan se på det på sikt for å forenkle programmet noe.
    Plan for videre arbeid er å opprette søkefunksjon
    for å finne ting i biblioteket og vise statusen
    på den samt legge til hvem som låner boken.
    For øyeblikket er funksjon for å legge til bok og
    film strippet ned for å spare tid ved testing.

--- library-code.py
+++ library-code.py
@@ -1,11 +1,11 @@
 from typing import Dict, List
-import uuid
+#import uuid
 from datetime import datetime as dt
 from dateutil.relativedelta import relativedelta
 
 class LibraryItem:
-    def __init__(self, GUID: str, title: str, description: str, amount: int, genre: str, publication_year: int, language: str, borrowed_by: str, borrowed_date: dt, location: str):
-        self.GUID = None                            # Unik identifikator i biblioteket for å holde styr på duplikater, None ved initialisering
+    def __init__(self, title: str, description: str, amount: int, genre: str, publication_year: int, language: str, borrowed_by: str, borrowed_date: dt, location: str):
+        #self.GUID = None                            # Unik identifikator i biblioteket for å holde styr på duplikater, None ved initialisering
         self.title = title                          # Tittel på bok eller film
         self.description = description              # Beskrivelse på bok eller film
         self.amount = amount                        # Antall - Lagt til for fremtidig utvidelse av funksjon for å vise om en har flere av samme
@@ -26,8 +26,9 @@ class LibraryItem:
     def borrow_item(self) -> None:
         if self.due_date is None:
             todays_date = dt(year=dt.now().year, month=dt.now().month,day=dt.now().day)
-            print(todays_date)
-            self.due_date = todays_date + relativedelta(months=1, days=1) # Dagens dato klokken 00:00 pluss en dag = en måned frem i tid. Boken må være inne innen stengetid dagen før
+            self.due_date = todays_date + relativedelta(months=1, days=1,seconds=-1) # Lever inn boken på slutten av dagen, en måned frem i tid
+            print(f"Du låner nå {self.title}")
+            print(f"Vennligste lever den tilbake innen: {self.due_date}")
         else:
             print("Denne er allerede lånt ut") 
             #TODO Mulighet for å legge inn noe error handling i stedet for en string? Hvordan blir implementasjonen senere?
@@ -42,86 +43,264 @@ class LibraryItem:
             return
         
 class Book(LibraryItem):
-    def __init__(self, title: str, author: str, isbn: str, pages: int):
-        super().__init__(title)
+    def __init__(self, title: str, description: str, amount: int, genre: str, publication_year: int, language: str, borrowed_by: str, borrowed_date: dt, location: str, author: str, isbn: str, pages: int):
+        super().__init__(title, description, amount, genre, publication_year, language, borrowed_by, borrowed_date, location)
         self.author = author        # Forfatter av boken
         self.isbn = isbn            # ISBN referanse
         self.pages = pages          # Antall sider i boken
 class Movie(LibraryItem):
-    def __init__(self, title: str, director: str, id: str, length:str, IMDB_rating:float, subtitles:str):
-        super().__init__(title)
-        self.director = director    # Direktør
+    def __init__(self, title: str, description: str, amount: int, genre: str, publication_year: int, language: str, borrowed_by: str, borrowed_date: dt, location: str, director: str, id: str, length:str, IMDB_rating:float, subtitles:str):
+        super().__init__(title, description, amount, genre, publication_year, language, borrowed_by, borrowed_date, location)
+        self.director = director    # Direktør 
         self.id = id                # IMDB ID referanse
         self.length = length
         self.IMDB_rating = IMDB_rating
         self.subtitles = subtitles
-        
+
 class Library:
     def __init__(self):
         self.items: Dict[str, LibraryItem] = {}
         
     def add_item(self, item) -> None:
-        #TODO: Legg til gjenstanden i biblioteket. Hva skjer hvis det finnes en duplikat?
+        item_key = define_library_key(item) # Legg til pretekst for å holde bok og film unik i keynavn i dictionaryen
+        if item_key in self.items:
+            print(f"Objekt er allerede i biblioteket: {item.title}")
+        else:
+            self.items[item_key] = item
+        return
+    
+    def remove_item(self, item) -> None:
+        item_key = define_library_key(item) # Hent aktuell key basert på klassen til item.
+        if item_key in self.items:
+            self.items.pop(item_key) # Fjern item_key fra biblioteket
+        else:
+            print(f"Kan ikke finne {item} i biblioteket")
+        return
+    
+    def find_books(self):
+        books = [b for b in self.items.values() if (isinstance(b,Book)) & (b.due_date == None)] # Hent alle bøker i biblioteket ved å sjekke verdien til hver oppføring og match den opp mot klassen.
+        print("\nLister opp alle bøker:\n----------------------")
+        i = 0
+        for b in books:
+            i += 1
+            print(f"{i}. {b.title}")
+        return books
+
+    def find_movies(self):
+        movies = [m for m in self.items.values() if (isinstance(m,Movie)) & (m.due_date == None)] # Hent alle filmer i biblioteket ved å sjekke verdien til hver oppføring og match den opp mot klassen.
+        print("\nLister opp alle filmer:\n---------------------")
+        i = 0
+        for m in movies:
+            i += 1
+            print(f"{i}. {m.title}")
+        return movies
+    
+    def find_books_borrowed(self):
+        books = [b for b in self.items.values() if (isinstance(b,Book)) & (b.due_date != None)] # Hent alle utlånte bøker i biblioteket ved å sjekke verdien til hver oppføring og match den opp mot klassen.
+        print("\nLister opp alle bøker:\n----------------------")
+        i = 0
+        for b in books:
+            i += 1
+            print(f"{i}. {b.title}")
+        return books
+
+    def find_movies_borrowed(self):
+        movies = [m for m in self.items.values() if (isinstance(m,Movie)) & (m.due_date != None)] # Hent alle utlånte filmer i biblioteket ved å sjekke verdien til hver oppføring og match den opp mot klassen.
+        print("\nLister opp alle filmer:\n---------------------")
+        i = 0
+        for m in movies:
+            i += 1
+            print(f"{i}. {m.title}")
+        return movies
+    def library_Statistics(self):
+        print("Denne funksjonen er ikke definert enda")
         pass
     
-    def remove_item(self, GUID) -> None:
-        # 
-        #TODO: Fjern gjenstanden fra biblioteket
-        pass
-    
-    #TODO: Lag to nye metoder for å finne lister med tilgjengelige bøker og filmer.
-    
-    """
-    # list comprehensions
-    electronics = [p for p in Produkter if isinstance(p, Electronics)]
-    clothing = [p for p in Produkter if isinstance(p, Clothing)]
 
-    #print all elektronikk:
-    print("printer alle elektronikk produkter:")
-    for produkt in electronics:
-        print(produkt.get_info())
-
-    #print all clothing:
-    print("\nPrint alle klær produkter:")
-    for clothes in clothing:
-        print(clothes.get_info())
+def define_library_key(item):
     """
-    
+    Oppsummering:
+    Lager unikt navn for hver type klasse som skal inn i biblioteket.
+    Hvis det er en bok så får du "Bok - <tittel på bok>".
+    På denne måten unngår vi å få duplikater mellom bok og film.
+
+    Argumenter:
+        item (class): får inn klasse objekt med enten bok eller film.
+
+    Returnerer:
+        string: Returnerer definert oppsett for å matche key i biblioteket
+                for å unngå duplikater mellom bok og film. 
+    """
+
+    match str(item.__class__.__name__): # Match klassen mot ønsket pretekst til key i dictionary til biblioteket
+        case "Book":
+            return "Bok - " + item.title
+        case "Movie":
+            return "Film - " + item.title
+
 def add_book(library):
-    #TODO: Implementer funksjonen for å legge til en bok.
-    # Hent input fra brukeren og kall library.add_item()
-    print("Add book ikke implementert")
+    """
+    title = input("Skriv inn tittelen på boken: ")
+    description = input("Skriv inn en beskrivelse på boken: ")
+    amount = 0 # Lagt til for fremtidig utvidelse
+    genre = input("Skriv inn sjanger: ")
+    publication_year = int(input("Skriv inn publiseringsår: "))
+    language = input("Skriv inn tilgjengelig språk: ")
+    borrowed_by = None # Kan vi fjerne denne og neste ved å bare definere den i klassen, men ikke hente inn input? skal kun settes ved lån i funksjoner.
+    borrowed_date = None
+    location = None # Lagt til for fremtidig utvidelse
+    author = input("Skriv inn forfatter: ")
+    isbn = input("Skriv inn isbn nummer: ")
+    pages = int(input("Skriv inn antall sider: "))
+    """
+    ## Fast innlegging av verdier for testing
+    title = input("Skriv inn tittelen på boken: ")
+    description = input("Skriv inn en beskrivelse på boken: ")
+    amount = 0 # Lagt til for fremtidig utvidelse
+    genre = "Sjanger ikke lagt til"
+    publication_year = "Publiseringsår"
+    language = "Norsk og Engelsk"
+    borrowed_by = None # Kan vi fjerne denne og neste ved å bare definere den i klassen, men ikke hente inn input? skal kun settes ved lån i funksjoner.
+    borrowed_date = None
+    location = None # Lagt til for fremtidig utvidelse
+    author = "Forfatter"
+    isbn = "12354"
+    pages = 999
+
+    library.add_item(Book(title, description, amount, genre, publication_year, language, borrowed_by, borrowed_date, location, author, isbn, pages))
 
 def add_movie(library):
-    #TODO: Implementer funksjonen for å legge til en film.
-    # Hent input fra brukeren og kall library.add_item()
-    print("Add movie ikke implementert")
+    """
+    title = input("Skriv inn tittelen på filmen: ")
+    description = input("Skriv inn en beskrivelse på filmen: ")
+    amount = 0 # Lagt til for fremtidig utvidelse
+    genre = input("Skriv inn sjanger: ")
+    publication_year = int(input("Skriv inn publiseringsår: "))
+    language = input("Skriv inn tilgjengelig språk: ")
+    borrowed_by = None # Kan vi fjerne denne og neste ved å bare definere den i klassen, men ikke hente inn input? skal kun settes ved lån i funksjoner.
+    borrowed_date = None
+    location = None # Lagt til for fremtidig utvidelse
+    director = input("Skriv inn direktør: ")
+    length = input("Skriv inn spilletid som f.eks: 1h 52m : ")
+    id = input("Skriv inn IMDB nummer: ")
+    IMDB_rating = int(input("Skriv inn antall sider: "))
+    subtitles = input("Skriv inn tilgjengelig teksting: ")
+    """
+    title = input("Skriv inn tittelen på filmen: ")
+    description = input("Skriv inn en beskrivelse på filmen: ")
+    amount = 0 # Lagt til for fremtidig utvidelse
+    genre = "midlertidig ikke med"
+    publication_year = 1234
+    language = "midlertidig ikke med"
+    borrowed_by = None # Kan vi fjerne denne og neste ved å bare definere den i klassen, men ikke hente inn input? skal kun settes ved lån i funksjoner.
+    borrowed_date = None
+    location = None # Lagt til for fremtidig utvidelse
+    director = "midlertidig ikke med"
+    length = "1h 52m"
+    id = 12345
+    IMDB_rating = 8.6
+    subtitles = "midlertidig ikke med"
+    library.add_item(Movie(title, description, amount, genre, publication_year, language, borrowed_by, borrowed_date, location, director, id, length, IMDB_rating, subtitles))
 
 def borrow_item(library):
-    #TODO: Implementer funksjonen for å låne en gjenstand.
-    # Hent input fra brukeren for å finne gjenstanden og kall library_item.borrow_item()
-    print("Borrow item ikke implementert")
-    
+    """
+    Oppsummering:
+    Funksjon for å låne en gjenstand fra biblioteket.
+    Spørr først brukeren om hvilken type gjenstand som skal lånes (bok eller film)
+    Deretter list opp tilgjengelige bøker med nummerering, hvis det er flere enn 10 alternativer
+    spør brukeren om å søke etter tittel.
+    Hent input fra brukeren for å finne gjenstanden og kall LibraryItem.borrow_item() funksjonen i klassen.
+
+    """
+    exit_requested = False
+    global borrow_list
+    while not exit_requested:
+        try:
+            print("\nMeny:")
+            print("1. Lån en bok")
+            print("2. Lån en film")
+            print("0. Avslutt")
+            
+            choice = -99
+            while (choice < 0) or (choice > 2): # Error handling hvis feil valg må inn.
+                choice = int(input("Velg et alternativ: ").strip())
+                match choice:
+                    case 1:
+                        print("\nVelg en av bøkene nedenfor:")
+                        borrow_list = library.find_books() # Printer listen over bøker og returnerer listen.
+                    case 2:
+                        print("\nVelg en av bøkene nedenfor:")
+                        borrow_list = library.find_movies() # Printer listen over filmer og returnerer listen.
+                    case 0:
+                        print("Avslutter låning av bok…")
+                        exit_requested = True
+                        return
+                    case _:
+                        print("ikke ett gyldig valg, velg mellom 0, 1 eller 2")
+
+            choice = int(input("Velg nummeret du vil låne: ").strip())-1
+            borrow_item = borrow_list[choice]
+            borrow_item.borrow_item()
+
+        except ValueError as e:
+            print(f"[Inputfeil] {e}")
+    #TODO: Implementer søkefunksjon etter hva du skal låne. F.eks som alternativ 3. Søk etter bok, 4. Søk etter film
+
 def return_item(library):
-    #TODO: Implementer funksjonen for å returnere en gjenstand.
-    # Hent input fra brukeren for å finne gjenstanden og kall library_item.return_item()
-    print("Return item ikke implementert")
+    """
+    Oppsummering:
+    Funksjon for å levere tilbake en gjenstand i biblioteket.
+    Spørr først brukeren om hvilken type gjenstand som skal leveres tilbake (bok eller film)
+    Deretter list opp tilgjengelige objekter med nummerering.
+    Hent input fra brukeren for å finne gjenstanden og kall LibraryItem.return_item() funksjonen i klassen.
+
+    """
+    exit_requested = False
+    global borrowed_list
+    while not exit_requested:
+        try:
+            print("\nMeny:")
+            print("1. Returner en bok")
+            print("2. Returner en film")
+            print("0. Avslutt")
+            
+            choice = -99
+            while (choice < 0) or (choice > 2): # Error handling hvis feil valg må inn.
+                choice = int(input("Velg et alternativ: ").strip())
+                match choice:
+                    case 1:
+                        print("\nVelg en av bøkene nedenfor:")
+                        borrowed_list = library.find_books_borrowed() # Printer listen over bøker og returnerer listen.
+                    case 2:
+                        print("\nVelg en av bøkene nedenfor:")
+                        borrowed_list = library.find_movies_borrowed() # Printer listen over filmer og returnerer listen.
+                    case 0:
+                        print("Avslutter returnering av bok…")
+                        exit_requested = True
+                        return
+                    case _:
+                        print("ikke ett gyldig valg, velg mellom 0, 1 eller 2")
+
+            choice = int(input("Velg nummeret du vil levere tilbake: ").strip())-1
+            borrowed_item = borrowed_list[choice]
+            borrowed_item.return_item()
+
+        except ValueError as e:
+            print(f"[Inputfeil] {e}")
+    #TODO: Implementer søkefunksjon etter hva du skal låne. F.eks som alternativ 3. Søk etter bok, 4. Søk etter film
+
 
 def find_item(library):
-    #TODO: Implementer funksjonen for å finne en gjenstand i biblioteket.
     # Hent input fra brukeren, og søk etter tittel igjennom alle tilgjengelige gjenstander.
     # Legg med hvilken type gjenstand det er (bok/film/annet) i resultatet.
+    
+    
     print("Find item ikke implementert")
 
 def main():
     #Lag et bibliotek
     library = Library()
-    """
-    Produkter = []
-    Produkter.append(Electronics("Datamaskin","ZBook G14 Fury",15000,10,12))
-    Produkter.append(Electronics("Hodetelefoner","Senheiser xx",2000,5,12))
-    Produkter.append(Clothing("Genser","Marius genser",600,100,"M"))
-    """
+
     
     exit_requested = False
     
@@ -136,7 +315,7 @@ def main():
             print("6. Returner gjenstand")
             print("7. Finn gjenstand")
             print("0. Avslutt")
-            
+
             choice = input("Velg et alternativ: ").strip()
             
             if choice == "1":
@@ -144,13 +323,9 @@ def main():
             elif choice == "2":
                 add_movie(library)
             elif choice == "3":
-                #list_available_books(library)
-                #TODO: Implementer funksjonen for å liste tilgjengelige bøker
-                pass
+                library.find_books()
             elif choice == "4":
-                #list_available_movies(library)
-                #TODO: Implementer funksjonen for å liste tilgjengelige filmer
-                pass
+                library.find_movies()
             elif choice == "5":
                 borrow_item(library)
             elif choice == "6":
